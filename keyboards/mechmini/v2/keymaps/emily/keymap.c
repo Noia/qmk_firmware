1c417331f72a0ee01a3c9c9168cd0eed3ae57dbe
Emily S <emily+git@soldal.org>
2018-02-05 23:56:12 +0100

    Add minivan keymap. Also some other changes

--- keyboards/mechmini/v2/keymaps/emily/keymap.c
+++ keyboards/mechmini/v2/keymaps/emily/keymap.c
@@ -1,45 +1,71 @@
 #include "mechmini.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+extern keymap_config_t keymap_config;
 
 #define _BL 0
 #define _FN1 1
 #define _FN2 2
+#define KC_ KC_TRNS
+#define _______ KC_TRNS
+#define xxxxxxx KC_NO
+#define KC_BL_S #define KC_BL_S BL_STEP
 
+#define FN_ESC LT(_FN1, KC_ESC)
+#define FN1 LT(_FN1, KC_QUOT)
+#define FN2 MO(_FN2)
+#define S___P__A__C___E KC_SPC
+
+//_______________
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BL] = KEYMAP_2U_SPACE_ORTHO(
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-		LT(_FN1, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, LT(_FN1, KC_QUOT),
-		KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-		KC_LCTL, KC_NO, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(_FN2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  //--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, FN1,
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+	//--------+--------+--------+--------+--------+--------.--------+--------+--------+--------+--------+--------+
+		KC_LCTL, xxxxxxx, KC_LALT, KC_LGUI, KC_ENT,  S___P__A__C___E,  FN2,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
 	[_FN1] = KEYMAP_2U_SPACE_ORTHO(
-		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, KC_BSLS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, M(1), M(2), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
+  //--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_DEL,
+  //--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		_______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_BSLS, _______,
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	//--------+--------+--------+--------+--------+--------.--------+--------+--------+--------+--------+--------+
+		_______, _______, _______, _______, _______,     _______,      _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
 	[_FN2] = KEYMAP_2U_SPACE_ORTHO(
-	  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-		KC_TRNS, BL_TOGG, BL_STEP, BL_INC, BL_DEC, BL_BRTG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
-		KC_TRNS, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, KC_HOME, KC_END, KC_TRNS,
-		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY),
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+	  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		_______, BL_TOGG, BL_STEP, BL_INC,  BL_DEC,  BL_BRTG, _______, _______, _______, _______, _______, _______,
+	//--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+		_______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______, _______,
+	//--------+--------+--------+--------+--------+--------.--------+--------+--------+--------+--------+--------+
+		RESET, _______, _______, _______, _______,      _______,       _______, _______, _______, _______, _______),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 
-	switch (id) {
-		case 1:
-			if (record->event.pressed) {
-				return MACRO( D(LCTL), T(C), U(LCTL), END );
-			}
-			break;
-		case 2:
-			if (record->event.pressed) {
-				return MACRO( D(LCTL), T(V), U(LCTL), END );
-			}
-			break;
-	}
+	// switch (id) {
+	// 	case 1:
+	// 		if (record->event.pressed) {
+	// 			return MACRO( D(LCTL), T(C), U(LCTL), END );
+	// 		}
+	// 		break;
+	// 	case 2:
+	// 		if (record->event.pressed) {
+	// 			return MACRO( D(LCTL), T(V), U(LCTL), END );
+	// 		}
+	// 		break;
+	// }
 	return MACRO_NONE;
 }
 
@@ -53,39 +79,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
 }
 
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-
-	} else {
-
-	}
-
-}
+// void led_set_user(uint8_t usb_led) {
+//
+// 	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+//
+// 	} else {
+//
+// 	}
+//
+// 	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+//
+// 	} else {
+//
+// 	}
+//
+// 	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+//
+// 	} else {
+//
+// 	}
+//
+// 	if (usb_led & (1 << USB_LED_COMPOSE)) {
+//
+// 	} else {
+//
+// 	}
+//
+// 	if (usb_led & (1 << USB_LED_KANA)) {
+//
+// 	} else {
+//
+// 	}
+//
+// }
 
 enum function_id {
     SHIFT_ESC,
