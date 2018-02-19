6d5a55599d37754ca635e55da8880cf3281551c5
Emily S <emily+git@soldal.org>
2018-02-20 00:52:58 +0100

    Various keyboard changes

---
+++ keyboards/roadkit/keymaps/emily/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define ORTHO_BIG_ENTER( \
+    K00, K01, K02, K03, \
+    K10, K11, K12, K13, \
+    K20, K21, K22, K23, \
+    K30, K31, K32       \
+) \
+{ \
+    { K00,   K01,   K02,   K03 }, \
+    { K10,   K11,   K12,   K13 }, \
+    { K20,   K21,   K22,   K23 }, \
+    { K30,   K31,   K32,   KC_NO }  \
+}
+
+#endif
---
+++ keyboards/roadkit/keymaps/emily/keymap.c
@@ -0,0 +1,68 @@
+#include "roadkit.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+#define _BL 0
+#define _L1 1
+#define _L2 2
+#define _L3 3
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BL] = ORTHO_BIG_ENTER(
+      // +--------+--------+--------+--------+
+          TO(0),   TO(1),   TO(2),   TO(3),  \
+      // +--------+--------+--------+--------+
+          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   KC_KP_ENTER, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO
+  ),
+  [_L1] = ORTHO_BIG_ENTER(
+      // +--------+--------+--------+--------+
+          TO(0),   TO(1),   TO(2),   TO(3),  \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   KC_NO, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   _______, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO
+  ),
+  [_L2] = ORTHO_BIG_ENTER(
+      // +--------+--------+--------+--------+
+          TO(0),   TO(1),   TO(2),   TO(3),  \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   KC_NO, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   _______, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO
+  ),
+  [_L3] = ORTHO_BIG_ENTER(
+      // +--------+--------+--------+--------+
+          TO(0),   TO(1),   TO(2),   TO(3),  \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   KC_NO, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO,   _______, \
+      // +--------+--------+--------+--------+
+          KC_NO,   KC_NO,   KC_NO
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    return true;
+}
